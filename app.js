b8d89079a4d94c73734fe7bfb2bb3db09743fc8f
Jakub1888 <krasnan.jk@gmail.com>
2021-01-09 15:51:40 +0100

    Added To-do list

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+Vue.createApp({
+    data() {
+        return {
+            title: 'Your To-Do List',
+            newTodo: '',
+            todos: []
+        };
+    },
+    methods: {
+        addTodo() {
+            this.todos.push({
+                title: this.newTodo,
+                done: false
+            });
+            this.newTodo = '';
+        },
+        removeTodo(todo) {
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos.splice(todoIndex, 1);
+        },
+        allDone() {
+            this.todos.forEach(todo => {
+                todo.done = true;
+            });
+        }
+    },
+}).mount("#app");
